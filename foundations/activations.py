8565b16b3968f77e722547a09c68cc00b005c28c
Enrique Queipo de Llano <72671373+equeipodlb@users.noreply.github.com>
2026-05-09 17:12:59 +0200

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -14,4 +14,4 @@ class Solution:
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: max(0, z) element-wise
-        return np.array([float(max(0,i)) for i in z])
+        return np.maximum(0,z)
